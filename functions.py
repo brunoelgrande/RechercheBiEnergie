7035b313f260c955b4b3206f188e20f6c6d69d60
Bruno Gauthier <bruno.gauthier@gmail.com>
2022-09-30 15:10:09 -0400

    first commit

---
+++ functions.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Ensemble des fonctions utilisées pour la recheche BiÉnergie
+Pour utilisation par : rechercherBiEnergie.py
+
+Created on 2022-09-29
+@author: Bruno Gauthier
+"""
+import streamlit as st
+import time
+import re
+import pandas as pd
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+def importData(nom_fichier: str) -> pd.DataFrame:
+
+    return pd.read_csv(nom_fichier, index_col='index')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+def prepString(String: str) -> str:
+    # Remplace les parenthèses de type Wildcard par '.' pour RegEx
+    while(String.find('(') != -1):
+        String = String[:String.index('(')]+'.' + String[String.index(')')+1:]
+
+    # Éliminie les caractères spéciaux par "" et '*' par '.' pour WildCard RegEx
+    return String.replace('+', '').replace('-', '').replace('/', '').replace('*', '.')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+def rechercheRegEx(equip_prop: list[str], equip_liste: list[str]) -> list[bool]:
+
+    # Initialisation des listes
+    equip_format = ["", "", ""]
+    verif = [False, False, False]
+
+    # Remplacer le wildcard '*' de la liste CEE par le wildcard '.' de RegEx
+    for x in range(len(equip_liste)):
+        equip_liste[x] = prepString(equip_liste[x])
+
+    # Besoin que l'équipement proposé soit de la même longueur que l'élément vérifié de la liste
+    for x in range(len(equip_prop)):
+        equip_format[x] = prepString(equip_prop[x])
+        equip_format[x] = equip_format[x][:len(equip_liste[x])]
+
+    # Vérifier avec RegEx pour chacun des 3 équipements si match sur la rangée
+    for x in range(len(equip_format)):
+        if(re.match(equip_liste[x], equip_format[x])):
+            verif[x] = True
+
+    return verif
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+@st.cache
+def finddMatches(equip_prop: list[str], df_CEE: pd.DataFrame) -> pd.DataFrame:
+
+    liste_match = []   # [Index de liste, match condenseur, match evaporateur, match fournaise]
+
+    for i in df_CEE.index:
+        equip_liste = [df_CEE['Condenseur'].iloc[i],
+                       df_CEE['Evaporateur'].iloc[i],
+                       df_CEE['Fournaise'].iloc[i]]
+
+        verif = rechercheRegEx(equip_prop, equip_liste)
+
+        # Si au moins 1 match, on l'ajoute à la liste
+        if (verif[0] | verif[1] | verif[2]):
+            # création d'une liste de listes
+            liste_match.append([i, verif[0], verif[1], verif[2]])
+
+    # Création d'un df avec tous les matches
+    df_matches = pd.DataFrame(liste_match)
+    if (len(df_matches) > 0):
+        df_matches.columns = [
+            'index', 'Condenseur', 'Evaporateur', 'Fournaise']
+
+    return df_matches
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+def phraseAccompagnement(type_equip: str, df: pd.DataFrame) -> str:
+
+    texte = ""
+
+    listeMarque = df['Marque'].unique().tolist()
+
+    # S'il y a seulement 1 marque
+    if (len(listeMarque) == 1):
+        texte += f"Des {type_equip} de la marque {listeMarque[0]} pourraient accompagner la sélection."
+
+    # S'il y a plusieurs marques
+    else:
+        texte += f"Des {type_equip} de marques "
+
+        for i in range(len(listeMarque)):
+            texte += listeMarque[i]
+
+            if i < (len(listeMarque)-2):
+                texte += ", "
+
+            if i == (len(listeMarque)-2):
+                texte += " et "
+
+            if i == (len(listeMarque)-1):
+                texte += " pourraient accompagner la sélection."
+
+    return texte
---
+++ notes.md
@@ -0,0 +1,53 @@
+# À faire
+
+## Besoin d'améliorer et tester la recherche, surtout pour les termes avec des '(' et '-'
+
+-[x] Créer une fonction, vérifier si possible d'accélérer... -[x] Tester le système avec les cas compliqués
+
+## Besoin de faire recherche similaire en parallèle avec NEEP
+
+- importer données
+- faire recherche
+- sortir les résultats
+  - si la recherche est un succès
+  - proposition d'appareils si non
+
+## Potentiel de faire une application dans le style Streamlit
+
+- déploy avec Github sur Streamlit Cloud
+  https://towardsdatascience.com/deploy-a-public-streamlit-web-app-for-free-heres-how-bf56d46b2abe#:~:text=1.3.&text=To%20launch%20the%20app%20locally,full%20path%20to%20the%20file.
+
+Exemple intéressant avec menu déroulant (st.expander)
+
+https://data-science-at-swast-handover-poc-handover-yfa2kz.streamlitapp.com/
+
+[GitHub - Data-Science-at-SWAST/handover_poc: Proof of Concept for Handover Reporting](https://github.com/Data-Science-at-SWAST/handover_poc)
+
+# Vérif Énergy Star
+
+-[x] Si vérifié avec CEE, on doit avoir Labeled ENERGY STAR = YES
+_Normalement, j'ai importé seulement ceux identifié Yes_
+--> OK
+
+Si vérifié dans la liste du NEEP, on assume Energy star. (NEEP > Energy Star)
+
+- Besoin d'une autorisation pour downloader fichier, en attente
+- Sinon, vérifier avec JSébastien s'ils ont la liste complète du MERN ?
+
+### Référence emoji
+
+https://www.webfx.com/tools/emoji-cheat-sheet/
+
+## Comment mettre les fonctions dans au autre fichier ?
+
+from function import \*
+
+## Requirements
+
+Fichier requirements.txt
+https://stackoverflow.com/questions/66899666/how-to-install-from-requirements-txt
+
+## Authentification
+
+Si besoin d'ajouter un processus d'authentification, une librairie existe : streamlit-authentificator
+https://youtu.be/JoFGrSRj4X4
